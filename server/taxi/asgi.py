26c89642d3599b72e3e4d25cd32aa4cde4a273b2
M. Brock Podgurski <mbpod10@gmail.com>
2023-03-29 20:20:23 -0400

    just realized the wrong django version

--- server/taxi/asgi.py
+++ server/taxi/asgi.py
@@ -10,11 +10,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import path 
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
 
+from trips.consumers import TaxiConsumer
+
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': URLRouter([
+        path('taxi/', TaxiConsumer.as_asgi()),
+    ]),
 })
