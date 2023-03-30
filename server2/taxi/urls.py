77a8115889ee188c1caa82d5a47e5b9d4d37a059
M. Brock Podgurski <mbpod10@gmail.com>
2023-03-29 20:25:58 -0400

    restart project

--- server2/taxi/urls.py
+++ server2/taxi/urls.py
@@ -1,7 +1,7 @@
 """taxi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
-from trips.views import SignUpView, LogInView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
-    path('api/log_in/', LogInView.as_view(), name='log_in'), 
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/trip/', include('trips.urls', 'trip',)),
 ]
